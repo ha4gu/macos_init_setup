589cd744c81f4ba4629b93d840dad9a1836077a6
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-04-10 21:56:22 +0900

    mod

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,57 +4,68 @@ set -eu
 # sudo
 echo "後続の処理のために一度sudoを実行"
 sudo echo "start"
+echo
 
 # Command Line Toolsのインストール
 xcode-select --install
 read -p "Hit enter after finishing installation of Command Line Tools: "
+# 完全自動化参考: https://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
 echo "Command Line Toolsのインストールが完了"
-# 完全に自動化したい場合の参考:
-# macos - How can I install the Command Line Tools completely from the command line? - Ask Different
-# https://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
+echo
 
 # GitHubからmacos_init_setupをclone
 mkdir ~/src
 git clone https://github.com/ha4gu/macos_init_setup.git ~/src/macos_init_setup
-SETUPDIR="~/src/macos_init_setup"
+SETUPDIR="${HOME}/src/macos_init_setup"
 echo "macos_init_setupのcloneが完了"
+echo
 
 # 必須ディレクトリの作成
 bash "${SETUPDIR}/mkdir.sh"
 echo "必須ディレクトリの作成が完了"
+echo
 
 # ~/.sshの処理
 bash "${SETUPDIR}/ssh.sh"
 echo "sshの処理が完了"
+echo
 
 # Karabiner-Elementsのjson配備
 mkdir -p ~/.config/karabiner/assets/
 ln -s "${SETUPDIR}/karabiner" ~/.config/karabiner/assets/complex_modifications
 echo "Karabiner-Elementsのjson配備が完了"
+echo
 
 # 各種ターミナルから実行する設定群
 bash "${SETUPDIR}/variousconf.sh"
 echo "各種設定コマンドの実行が完了"
+echo
 
 # Homebrewのインストール
 bash "${SETUPDIR}/homebrew.sh"
 echo "Homebrewのインストールが完了"
+echo
 
 # brew bundleの実行
 cp "${SETUPDIR}/Brewfile" ~/.Brewfile
 bash "${SETUPDIR}/brewbundle.sh"
 echo "brew bundleの実行が完了"
+echo
 
 # dotfilesの展開
 bash "${SETUPDIR}/dotfiles.sh"
 echo "dotfilesの展開が完了"
+echo
 
 # ログインシェルの変更
 bash "${SETUPDIR}/loginshell.sh"
 echo "ログインシェルの変更が完了"
+echo
 
 # bash-completionへの追加
 bash "${SETUPDIR}/completion.sh"
 echo "bash-completionへの追加が完了"
+echo
 
 echo "糸冬"
+echo
