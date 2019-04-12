c64e4930f5777cd3cc5767f1ea7e0bab683ddeb2
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-04-12 21:59:18 +0900

    add installation of virtualbox-extension-pack

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,6 +56,7 @@ echo
 set +e
 brew cask install virtualbox || (read -p "Hit enter after allow kext: " && brew cask install virtualbox)
 set -e
+brew cask install virtualbox-extension-pack
 brew cask install vagrant
 brew cask install vagrant-manager
 echo "brew bundle外のcask install実行が完了"
