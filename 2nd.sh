5afc6d69e580409fbb36337a1d22dc329302ec53
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-04-20 11:13:12 +0900

    Separate main script into two scripts

---
+++ 2nd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+export SETUPDIR="${HOME}/src/macos_init_setup"
+
+# anyenvのセットアップ
+bash "${SETUPDIR}/anyenv.sh"
+echo "anyenvのセットアップが完了"
+echo
+
+# rbenvのセットアップ
+ln -s "${SETUPDIR}/default-gems" "$(rbenv root)/default-gems"
+bash "${SETUPDIR}/rbenv.sh"
+echo "rbenvのセットアップが完了"
+
+# nodenvのセットアップ
+bash "${SETUPDIR}/nodenv.sh"
+echo "nodenvのセットアップが完了"
+
+# pyenvのセットアップ
+bash "${SETUPDIR}/pyenv.sh"
+echo "pyenvのセットアップが完了"
+
+# レポジトリのダウンロード
+bash "${SETUPDIR}/repos.sh"
+echo "レポジトリのダウンロードが完了"
+
+# Mackup
+ln -s "${SETUPDIR}/Mackup.cfg" "${HOME}/.mackup.cfg"
+echo "Mackup設定ファイルのリンクが完了"
+
+echo "糸冬"
+echo
--- bootstrap.sh
+++ bootstrap.sh
@@ -79,31 +79,5 @@ bash "${SETUPDIR}/completion.sh"
 echo "bash-completionへの追加が完了"
 echo
 
-# anyenvのセットアップ
-bash "${SETUPDIR}/anyenv.sh"
-echo "anyenvのセットアップが完了"
-echo
-
-# rbenvのセットアップ
-ln -s "${SETUPDIR}/default-gems" "$(rbenv root)/default-gems"
-bash "${SETUPDIR}/rbenv.sh"
-echo "rbenvのセットアップが完了"
-
-# nodenvのセットアップ
-bash "${SETUPDIR}/nodenv.sh"
-echo "nodenvのセットアップが完了"
-
-# pyenvのセットアップ
-bash "${SETUPDIR}/pyenv.sh"
-echo "pyenvのセットアップが完了"
-
-# レポジトリのダウンロード
-bash "${SETUPDIR}/repos.sh"
-echo "レポジトリのダウンロードが完了"
-
-# Mackup
-ln -s "${SETUPDIR}/Mackup.cfg" "${HOME}/.mackup.cfg"
-echo "Mackup設定ファイルのリンクが完了"
-
-echo "糸冬"
+echo "シェルを再起動後、次のスクリプトを実行してください"
 echo
