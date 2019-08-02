72255e4bcc89d32a9865d0b90c96e50938d42875
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-08-02 11:58:13 +0900

    Update Node.js version to 10.16.1

--- nodenv.sh
+++ nodenv.sh
@@ -3,7 +3,7 @@
 
 git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
 
-nodenv install 10.15.3
-nodenv global 10.15.3
+nodenv install 10.16.1
+nodenv global 10.16.1
 nodenv rehash
 npm completion > /usr/local/etc/bash_completion.d/npm
