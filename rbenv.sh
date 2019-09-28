064ca65b273a56762b1567f24af6a5a7c21e4dfe
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-09-28 18:37:41 +0900

    Update rbenv process

--- rbenv.sh
+++ rbenv.sh
@@ -5,19 +5,22 @@ git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/
 
 rbenv install 2.3.3
 rbenv install 2.5.5
-rbenv install 2.6.3
+rbenv install 2.6.4
 
 rbenv global 2.3.3
 gem update --system
+gem install fileutils --default
 gem update
 gem install did_you_mean -v '<1.1'
 
 rbenv global 2.5.5
 gem update --system
+gem install fileutils --default
 gem update
 
-rbenv global 2.6.3
+rbenv global 2.6.4
 gem update --system
+gem install fileutils --default
 gem update
 
 rbenv global system
