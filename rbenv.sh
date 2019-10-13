82c5cf1784e9972c5a19a5a5b5cd4ae42e62f0bd
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-10-13 17:06:52 +0900

    Update ruby versions

--- rbenv.sh
+++ rbenv.sh
@@ -3,9 +3,9 @@
 
 git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/rbenv-default-gems
 
-rbenv install 2.3.3
-rbenv install 2.5.5
-rbenv install 2.6.4
+RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline) --with-openssl-dir=$(brew --prefix openssl)" rbenv install 2.3.3
+rbenv install 2.5.7
+rbenv install 2.6.5
 
 rbenv global 2.3.3
 gem update --system
@@ -13,12 +13,12 @@ gem install fileutils --default
 gem update
 gem install did_you_mean -v '<1.1'
 
-rbenv global 2.5.5
+rbenv global 2.5.7
 gem update --system
 gem install fileutils --default
 gem update
 
-rbenv global 2.6.4
+rbenv global 2.6.5
 gem update --system
 gem install fileutils --default
 gem update
