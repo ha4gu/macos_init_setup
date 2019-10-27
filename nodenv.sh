f5c10f5826885f87c235308bae383ba00eface63
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-10-27 15:59:37 +0900

    Update node versions

--- nodenv.sh
+++ nodenv.sh
@@ -3,8 +3,8 @@
 
 git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
 
-nodenv install 10.16.3
-nodenv install 12.12.0
-nodenv global 12.12.0
+nodenv install 10.17.0
+nodenv install 12.13.0
+nodenv global 12.13.0
 nodenv rehash
 npm completion > /usr/local/etc/bash_completion.d/npm
