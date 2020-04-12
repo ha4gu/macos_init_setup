3c640b69e36930f3b55e1960f980c4305d9971bb
ha4gu <9391978+ha4gu@users.noreply.github.com>
2020-04-12 12:15:15 +0900

    Add ruby 2.7.1

--- rbenv.sh
+++ rbenv.sh
@@ -6,6 +6,7 @@ git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/
 rbenv install 2.3.3 # for AtCoder
 rbenv install 2.6.3 # for paiza
 rbenv install 2.6.6
+rbenv install 2.7.1
 
 rbenv global 2.3.3
 gem update --system
@@ -20,6 +21,12 @@ gem install fileutils --default
 gem update
 
 rbenv global 2.6.6
+gem update --system
+gem install fileutils --default
+gem install pg -- --with-pg-config=/usr/local/opt/postgresql@9.6/bin/pg_config
+gem update
+
+rbenv global 2.7.1
 gem update --system
 gem install fileutils --default
 gem install pg -- --with-pg-config=/usr/local/opt/postgresql@9.6/bin/pg_config
