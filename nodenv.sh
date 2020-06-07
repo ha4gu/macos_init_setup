7b6ff2039b5ff00e92328117418ec4d7bc1910dd
ha4gu <9391978+ha4gu@users.noreply.github.com>
2020-06-07 17:35:00 +0900

    Update to use Node.js 12.18.0

--- nodenv.sh
+++ nodenv.sh
@@ -3,12 +3,9 @@
 
 git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
 
-nodenv install 10.14.2
-nodenv install 12.16.3
-nodenv global  12.16.3
+nodenv install 12.18.0
+nodenv global  12.18.0
 nodenv rehash
-npm install -g npm
-npm install -g firebase-tools
-npm install -g gatsby-cli
+npm install -g npm firebase-tools gatsby-cli
 nodenv rehash
 npm completion > /usr/local/etc/bash_completion.d/npm
