28704cfa3787d0008b1d7b8f12ea1a1bd4f12d2d
ha4gu <9391978+ha4gu@users.noreply.github.com>
2021-01-01 17:04:50 +0900

    インストールするRubyのバージョンを整理

--- rbenv.sh
+++ rbenv.sh
@@ -3,17 +3,9 @@
 
 git clone https://github.com/rbenv/rbenv-default-gems.git $(rbenv root)/plugins/rbenv-default-gems
 
-rbenv install 2.3.3 # for AtCoder
 rbenv install 2.6.6
-rbenv install 2.7.0 # for paiza
-rbenv install 2.7.1
-
-rbenv global 2.3.3
-gem update --system
-gem install fileutils --default
-gem update
-gem install did_you_mean -v '<1.1'
-gem install bigdecimal -v '<2'
+rbenv install 2.7.1 # for AtCoder and paiza
+rbenv install 2.7.2
 
 rbenv global 2.6.6
 gem update --system
@@ -21,12 +13,12 @@ gem install fileutils --default
 gem install pg -- --with-pg-config=/usr/local/opt/postgresql@9.6/bin/pg_config
 gem update
 
-rbenv global 2.7.0 # for paiza
+rbenv global 2.7.1 # for AtCoder and paiza
 gem update --system
 gem install fileutils --default
 gem update
 
-rbenv global 2.7.1
+rbenv global 2.7.2
 gem update --system
 gem install fileutils --default
 gem install pg -- --with-pg-config=/usr/local/opt/postgresql@9.6/bin/pg_config
