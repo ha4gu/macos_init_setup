6725a0b123b7f7c21e14d9fe3fbf75205bad1364
ha4gu <9391978+ha4gu@users.noreply.github.com>
2021-01-01 17:04:24 +0900

    公式サイトの案内に沿ってHomebrewの導入コマンドを更新

--- homebrew.sh
+++ homebrew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Homebrewのインストールを実行する
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
