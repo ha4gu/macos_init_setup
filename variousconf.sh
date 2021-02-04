11563950978115b85b938c0c23fc2d97430ef828
Yuta HASHIGUCHI <9391978+ha4gu@users.noreply.github.com>
2021-02-04 12:41:21 +0900

    スクリーンショットに関する設定項目を追加して内容を整理

--- variousconf.sh
+++ variousconf.sh
@@ -4,10 +4,10 @@
 # 隠しファイルを常に表示させる
 defaults write com.apple.finder AppleShowAllFiles -boolean true
 
-# SMBで.DS_Storeを作成させない
+### SMB関連
+# .DS_Storeを作成させない
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -boolean true
-
-# SMBでパケット署名による暗号化の無効化
+# パケット署名による暗号化を無効にする
 cat << EOF > /tmp/nsmb.conf
 [default]
 signing_required=no
@@ -15,10 +15,16 @@ EOF
 echo "/etc/nsmb.confへの書き込みにsudo実行が必要"
 sudo mv /tmp/nsmb.conf /etc/.
 
-# スクリーンショットの保存場所をデスクトップからピクチャに変更する
-# 参考: https://pc-karuma.net/mac-customize-screencapture-location/
+### スクリーンショット関連 参考: https://qiita.com/ZEON/items/0bd778c18e0065d97f9e
+# ファイル形式をPNGからJPEGに変える
+defaults write com.apple.screencapture type jpg
+# ファイル名の接頭辞を "スクリーンショット" から "screenshot" に変える
+defaults write com.apple.screencapture name screenshot
+# 保存場所をデスクトップからピクチャに変更する
 defaults write com.apple.screencapture location ~/Pictures/
-
-# スクリーンショット取得時にウインドウの影をなくす
-# 参考: https://pc-karuma.net/mac-disable-drop-shadows-on-screenshots/
+# ウインドウの影をなくす
 defaults write com.apple.screencapture disable-shadow -boolean true
+# スクリーンショット取得時のフローティングサムネイル表示を無効にする
+defaults write com.apple.screencapture show-thumbnail -boolean false
+# スクリーンショットにマウスポインタを含める
+defaults write com.apple.screencapture showsCursor -boolean true
