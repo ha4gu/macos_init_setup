933aca467213c3462cadb43c3b3dce5e31d9f69f
Yuta HASHIGUCHI <9391978+ha4gu@users.noreply.github.com>
2022-01-01 22:33:24 +0900

    PostgreSQLのバージョンを9.6から13に変更

--- rbenv.sh
+++ rbenv.sh
@@ -7,13 +7,13 @@ rbenv install 2.7.2
 rbenv global 2.6.6
 gem update --system
 gem install fileutils --default
-gem install pg -- --with-pg-config=/usr/local/opt/postgresql@9.6/bin/pg_config
+gem install pg -- --with-pg-config=/usr/local/opt/postgresql@13/bin/pg_config
 gem update
 
 rbenv global 2.7.2
 gem update --system
 gem install fileutils --default
-gem install pg -- --with-pg-config=/usr/local/opt/postgresql@9.6/bin/pg_config
+gem install pg -- --with-pg-config=/usr/local/opt/postgresql@13/bin/pg_config
 gem update
 
 rbenv global system
