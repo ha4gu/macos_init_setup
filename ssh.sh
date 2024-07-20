e7cf45c9d2738bba987fcb51dc8cfeecb37650cb
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2024-07-20 22:59:40 +0900

    sshの鍵形式をrsaからed25519に変更

--- ssh.sh
+++ ssh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ~/.sshの処理を実行する
 
-chmod 400 ~/.ssh/id_rsa
-chmod 444 ~/.ssh/id_rsa.pub
+chmod 400 ~/.ssh/id_ed25519
+chmod 444 ~/.ssh/id_ed25519.pub
 git clone git@github.com:ha4gu/ssh.git ~/ssh
 mv ~/.ssh/* ~/ssh/.
 rmdir ~/.ssh
