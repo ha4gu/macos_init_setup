dedd538c1de0a9dc2533fb297aa5c50aeb0ce194
Yuta Hashiguchi <9391978+ha4gu@users.noreply.github.com>
2024-07-20 23:41:22 +0900

    ログインシェル変更手順をApple Silicon対応

--- loginshell.sh
+++ loginshell.sh
@@ -3,5 +3,5 @@
 
 # 参考: https://rcmdnk.com/blog/2015/05/25/computer-mac-bash-zsh/
 echo "/etc/shellsへの書き込みにsudo実行が必要"
-sudo sh -c "echo /usr/local/bin/bash >> /etc/shells"
-chsh -s /usr/local/bin/bash
+sudo sh -c "echo $(brew --prefix)/bin/bash >> /etc/shells"
+chsh -s $(brew --prefix)/bin/bash
